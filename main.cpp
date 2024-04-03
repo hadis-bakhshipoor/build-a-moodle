742007985bd73aec73364fb25b719a1a64b0dd7f
hadis <hadis.bakhship@gmail.com>
2024-04-03 17:30:01 +0330

    Add first class (student)

--- main.cpp
+++ main.cpp
@@ -3,7 +3,17 @@
 extern string name , role;
 
 int main(){
+    string lesson_name = "ap";
+    string teacher_name = "Nicknaddaf";
+    string student_name = "Hadis Bakhshipoor";
+    string student_taklif = "taklif1";
+
+    student daneshjo;
+    // daneshjo.read_grade_from_file(lesson_name, teacher_name, student_name);
+    login();
+    cout << role << endl;
    
-   
+
+    // daneshjo.read_takalif_from_file(lesson_name, teacher_name, student_name, student_taklif);
 
 }
--- project.cpp
+++ project.cpp
@@ -2,13 +2,65 @@
 
 string name , role;
 
-void login() { }
+void login() {
+    
+    cout<<"Enter your full name"<<endl;
+    getline(cin, name);
+    cout<<"Enter ypur rull"<<endl;
+    cin >> role;
+}
 
 void student::read_grade_from_file(string lesson_name, string teacher_name, string full_name) {
 
+    string student_full_name = "nothing";
+    string file_name, student_grade;
+
+    std::filesystem::path cwd = std::filesystem::current_path();
+    file_name = lesson_name + "_" + teacher_name + ".txt";
+    std::filesystem::path file_path = cwd / "grade" / file_name;
+    
+    ifstream file(file_path, ios::in);
+    if(!file) cout<<"your lesson name or teacher name is wrong";
+
+    while(!file.eof()) {
+        string student_name, student_last_name;
+        file >> student_name >> student_last_name >> student_grade;
+        student_full_name = student_name + " " + student_last_name; 
+
+        if(student_full_name == (full_name + ":")) {
+            cout << full_name << ", your grade is: " << student_grade;
+            break;
+        }   
+    }
+
+    if(student_full_name != (full_name + ":"))
+        cout<<"Your name is wrong or your name dose not found";
 
 }
 
 void student::read_takalif_from_file(string lesson_name, string teacher_name, string full_name, string number_of_taklif) {
     
+    string student_full_name = "nothing";
+    string file_name, student_grade;
+
+    std::filesystem::path cwd = std::filesystem::current_path();
+    file_name = lesson_name + "_" + number_of_taklif + "_" + teacher_name + ".txt";
+    std::filesystem::path file_path = cwd / "takalif" / file_name;
+
+    ifstream file(file_path, ios::in);
+    if(!file) cout<<"your lesson name or teacher name is wrong";
+
+    while(!file.eof()) {
+        string student_name, student_last_name;
+        file >> student_name >> student_last_name >> student_grade;
+        student_full_name = student_name + " " + student_last_name; 
+
+        if(student_full_name == (full_name + ":")) {
+            cout << full_name << ", your grade for: " << number_of_taklif << " is: "<< student_grade;
+            break;
+        }   
+    }
+
+    if(student_full_name != (full_name + ":"))
+        cout<<"Your name is wrong or your name dose not found";
 }
